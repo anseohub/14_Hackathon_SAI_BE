cc7dc4eb675aa9e0f8b9dd076f5348709921e854
anseohub <natjgus02@gamil.com>
2026-08-20 16:52:46 +0900

    배포 오류 수정

--- src/main/java/com/sai/sai_api/Controller/TrainController.java
+++ src/main/java/com/sai/sai_api/Controller/TrainController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/train")
+@CrossOrigin(origins = "*")
 @RequiredArgsConstructor
 public class TrainController {
     private final TrainService trainService;
